25909f042e67331bcb617fe2d14862bb4c011f85
Evan Mann <ejmann900@gmail.com>
2023-12-16 23:58:55 -0700

    Finished part 1

--- Day 3/Day3_Part1/Source.cpp
+++ Day 3/Day3_Part1/Source.cpp
@@ -2,43 +2,80 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int sumPartNumbers(string fileName)
 {
-	vector<vector<char>> engineLayout;
+	int sumParts = 0;
+	bool validNum = false;
+
+	vector<string> engineLayout;
 	engineLayout.resize(140);
 
-	string line;
+	string line, strToCheck, strPart1 = "", strPart2 = "", strPart3 = "";
 
 	ifstream inputFile(fileName);
 
 	int lineNum = 0;
 
 	//Formatting the text file into a vector
-	while (getline(inputFile, line))
+	while (getline(inputFile, line)) 
 	{
-		engineLayout[lineNum].resize(line.length());
-
-		for (int i = 0; i < line.length(); i++)
-		{
-			char test = line[i];
-			engineLayout[lineNum][i] = line[i];
-		}
-
+		engineLayout[lineNum] = line;
 		lineNum += 1;
 	}
 
+	//looping over each line in the layout
+	for (int i = 0; i < engineLayout.size(); i++)
+	{
+		strPart1 = "", strPart2 = "", strPart3 = "";
 
+		int numIdx = 0;
+		int numLength;
+		while (true)
+		{
+			//finding the index of the number to be checked
+			numIdx = engineLayout[i].find_first_of("1234567890", numIdx);
+			if (numIdx == string::npos)
+				break;
+
+			//finding the length of the number being checked
+			numLength = engineLayout[i].find_first_not_of("1234567890", numIdx) - numIdx;
+
+			int curNum = stoi(engineLayout[i].substr(numIdx, numLength));
+
+
+			if (i > 0)
+				strPart1 = engineLayout[i - 1].substr(max(0, numIdx - 1), numLength + 2);
+
+			strPart2 = engineLayout[i].substr(max(0, numIdx - 1), numLength + 2);
+
+			if (i < 139)
+				strPart3 = engineLayout[i + 1].substr(max(0, numIdx - 1), numLength + 2);
+
+
+			strToCheck = strPart1 + strPart2 + strPart3;
+
+			if (strToCheck.find_first_of("*+@#=/-$%&") != string::npos)
+				sumParts += curNum;
+
+			numIdx += numLength;
+		}
+	}
 	
 
-	return 0;
+	return sumParts;
 }
 
 
 int main(void)
 {
-	sumPartNumbers("day3_PuzzleInput.txt");
+	int answer = sumPartNumbers("day3_PuzzleInput.txt");
+	cout << "The answer to day 3 part 1 is: " << answer;
 	return 0;
 }
+
+
+467..11...*.....35..6
